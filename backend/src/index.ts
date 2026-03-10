c848ac021df94e51f4b1fc30307ad79099368d76
Priyanshu Chaudhary <helloworld82025840@gmail.com>
2026-03-10 23:38:11 +0530

    fixing some bugs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const app = new Hono()
 
@@ -6,9 +8,22 @@ const app = new Hono()
 //   return c.text('Hello Hono!')
 // })
 
-app.post('/api/v1/signup', (c) => {
-  if(c)
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', async (c) => {
+  const prisma =new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body = await c.req.json()
+
+  const res = await prisma.user.create({
+    data:{
+      email:      body.email,
+      password:   body.password,
+      name:       body.name,
+    }
+  })
+
 })
 
 app.post('/api/v1/signin', (c) => {
