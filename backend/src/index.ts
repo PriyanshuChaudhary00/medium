7d886a7c62f2205ea7470b22128cbd6bb505d8e9
Priyanshu Chaudhary <helloworld82025840@gmail.com>
2026-03-11 00:58:55 +0530

    added signup and signin routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,80 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client'
+import { sign , verify} from "hono/jwt"
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
 
-// app.get('/', (c) => {
-//   return c.text('Hello Hono!')
-// })
+const app = new Hono<{
+  Bindings: {
+    PRISMA_DATABASE_URL: string;
+    JWT_SECRET: string;
+  }
+}>()
+
+app.use('/api/v1/blog/*', async (c , next)=>{
+  const header = await c.req.header("Authorization");
+  //@ts-ignore
+  const responce = await verify(header , c.env.JWT_SECRET)
+  if(responce.id){
+    await next()
+  }
+  else return c.json({msg:"error while signing in "} , 403)
+
+})
 
 app.post('/api/v1/signup', async (c) => {
-  const prisma =new PrismaClient({
-    //@ts-ignore
-    datasourceUrl: c.env.DATABASE_URL,
+  const prisma = new PrismaClient({
+    accelerateUrl: c.env.PRISMA_DATABASE_URL,
   }).$extends(withAccelerate())
   
   const body = await c.req.json()
 
-  const res = await prisma.user.create({
-    data:{
-      email:      body.email,
-      password:   body.password,
-      name:       body.name,
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email:      body.email,
+        password:   body.password,
+        name:       body.name,
+      }
+    })
+    const jwt = await sign({id:user.id} , c.env.JWT_SECRET)
+    return c.json({
+      jwt
+    })
+  } catch(e) {
+    console.log(e)
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  }
+})
+
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    accelerateUrl: c.env.PRISMA_DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body = await c.req.json()
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password
     }
   })
 
+  if (!user || user.password !== body.password) {
+    c.status(403);
+    return c.json({ error: "invalid credentials" });
+  }
+
+  const token = await sign({id:user.id} , c.env.JWT_SECRET)
+  return c.json({
+    jwt:token
+  })
 })
 
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
-})
+
+
 
 app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
